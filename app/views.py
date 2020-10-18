ad0af70ac3d1fc65c2f24b5c8e923cecf5393655
StacyWeru <stacyweru@gmail.com>
2020-10-18 05:59:04 +0300

    Added a route to the index.html file

--- app/views.py
+++ app/views.py
@@ -9,4 +9,5 @@ def index():
     View root page function that returns the index page and its data
     '''
 
-    return render_template ('index.html')
+    message = 'Hello News Api app'
+    return render_template ('index.html', message = message)
