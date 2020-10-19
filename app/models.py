69b9ef7c283790359d567ca475324377020921f5
StacyWeru <stacyweru@gmail.com>
2020-10-19 04:57:46 +0300

    Add class articles

---
+++ app/models.py
@@ -0,0 +1,13 @@
+class Article:
+
+    '''
+    Article class to define article objects
+    '''
+
+    def __init__(self,image,url,title,author,description,publishedAt):
+        self.image = image
+        self.url =image
+        self.title = title
+        self.author = author
+        self.description = description
+        self.pulishedAt = publishedAt
