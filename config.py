a5c56edbe12e432b80f824bc35ea8ee49b4451ce
StacyWeru <stacyweru@gmail.com>
2020-10-21 08:07:06 +0300

    Changed the application structure

--- config.py
+++ config.py
@@ -1,8 +1,11 @@
+import os
+
 class Config:
     '''
     General configuration parent class
     '''
-    pass
+     NEWS_API_SOURCE_URL='https://newsapi.org/v2/sources?apiKey={}'
+     NEWS_API_KEY=os.environ.get('NEWS_API_KEY')
 
 
 
@@ -25,3 +28,8 @@ class DevConfig(Config):
     '''
 
     DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
